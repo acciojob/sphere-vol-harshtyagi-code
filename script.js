96a5866c0fe56489da81faeb0e7753139520906e
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-25 10:44:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function volume_sphere(event) {
+function volume_sphere() {
     //Write your code here
-	event.preventDefault();
-	let item = parseFloat(document.getElementById('radius').value);
-  let vol = document.getElementById('volume');
-  vol.inputMode.value = none;
-	let result =  4/3 * Math.PI*item *item*item;
-  console.log(result);
+	const rad = document.getElementById("radius");
+const vol = document.getElementById("volume");
+const button = document.getElementById("submit");
+button.addEventListener("click", (event)=>{
+  event.preventDefault();
+  let volCircle = (4 * Math.PI * rad.value * rad.value * rad.value)/3;
+  vol.value = volCircle.toFixed(4);
+});
+} 
 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 	vol.value = result.toFixed(2);
 	} 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
