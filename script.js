95edbdc3aab249bc47c43dc9831075579a04b046
Harsh Tyagi <69513043+harshtyagi-code@users.noreply.github.com>
2024-01-25 10:41:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
-} 
+	event.preventDefault();
+	let item = parseFloat(document.getElementById('radius').value);
+  let vol = document.getElementById('volume');
+  vol.inputMode.value = none;
+	let result =  4/3 * Math.PI*item *item*item;
+  console.log(result);
+
+	vol.value = result.toFixed(2);
+	} 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
